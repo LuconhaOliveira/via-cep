1749cdc96780127907bb669b5d27d23de5a13d6f
LuconhaOliveira <lucaas.oliveira0254@gmail.com>
2025-04-08 17:18:10 -0300

    mais abstração

--- src/js/buscar-cep-numero.js
+++ src/js/buscar-cep-numero.js
@@ -1,20 +1,13 @@
-const elemCep = document.querySelector('#cep');
-const elemRua = document.querySelector('#rua')
-const elemComplemento = document.querySelector('#complemento')
-const elemBairro = document.querySelector('#bairro')
-const elemCidade = document.querySelector('#cidade')
-const elemEstado = document.querySelector('#estado')
-
 document.querySelector('#btnLimpar').addEventListener('click',()=>{
-    elemCep.value = "";
-    elemRua.value = "";
-    elemComplemento.value = "";
-    elemBairro.value = "";
-    elemCidade.value = "";
-    elemEstado.value = "";
+    document.querySelector('#cep').value = "";
+    document.querySelector('#rua').value = "";
+    document.querySelector('#complemento').value = "";
+    document.querySelector('#bairro').value = "";
+    document.querySelector('#cidade').value = "";
+    document.querySelector('#estado').value = "";
 });
 
-elemCep.addEventListener('keypress',(e)=>{
+document.querySelector('#cep').addEventListener('keypress',(e)=>{
     if(e.keyCode<48 || e.keyCode>57){
         e.preventDefault();
         alert("Por favor, insira apenas números!")
@@ -30,24 +23,35 @@ const obterDadosApi = async(cep)=>{
         alert("O CEP inserido está incorreto!");
         return;
     }
-    atribuirCampos(data);
+    document.querySelector('#rua').value = data.logradouro;
+    document.querySelector('#complemento').value = data.complemento;
+    document.querySelector('#bairro').value = data.bairro;
+    document.querySelector('#cidade').value = data.localidade;
+    document.querySelector('#estado').value = data.estado;
 }
 
 document.querySelector('#btnPesquisar').addEventListener('click',(e)=>{
     e.preventDefault();
 
-    if(elemCep.value.length!=8){
+    if(document.querySelector('#cep').value.length!=8){
         alert('Por favor, digite um CEP válido com 8 dígitos.')
         document.querySelector('#cep').value="";
         return;
     }
-    obterDadosApi(elemCep.value);
-});
+    
+    async()=>{
+        let urlViaCep = `https://viacep.com.br/ws/${document.querySelector('#cep').value}/json`
+        let response = await fetch(urlViaCep);
+        let data = await response.json()
 
-const atribuirCampos=(callback)=>{
-    elemRua.value = callback.logradouro;
-    elemComplemento.value = callback.complemento;
-    elemBairro.value = callback.bairro;
-    elemCidade.value = callback.localidade;
-    elemEstado.value = callback.estado;
-}
+        if(data.erro){
+            alert("O CEP inserido está incorreto!");
+            return;
+        }
+        document.querySelector('#rua').value = data.logradouro;
+        document.querySelector('#complemento').value = data.complemento;
+        document.querySelector('#bairro').value = data.bairro;
+        document.querySelector('#cidade').value = data.localidade;
+        document.querySelector('#estado').value = data.estado;
+    }
+});
